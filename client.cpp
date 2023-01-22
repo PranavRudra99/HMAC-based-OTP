e1eb3795fb5abaac6c19e57847912ee04f57595b
PranavRudra99 <pranavrudra1999@gmail.com>
2023-01-22 16:25:42 -0500

    Implemented server/client communication over TCP

--- client.cpp
+++ client.cpp
@@ -1,15 +1,24 @@
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <string_view>
 #include <array>
+#include <fstream>
 
 using namespace std;
 
-std::string CalcHmacSHA256(std::string_view decodedKey, std::string_view msg)
+std::string CalcHmacSHA(std::string_view decodedKey, std::string_view msg)
 {
     std::array<unsigned char, EVP_MAX_MD_SIZE> hash;
     unsigned int hashLen;
@@ -30,10 +39,81 @@ std::string CalcHmacSHA256(std::string_view decodedKey, std::string_view msg)
     return out.str();
 }
 
-int main(int argc, char **argv){
-    std::string key = "Pokemon";
-    std::string msg = "foo";
-    std::string_view key_view{key};
-    std::string_view msg_view{msg};
-    std::cout << CalcHmacSHA256(key_view, msg_view) << std::endl;
+void GenerateHexString(char str[], int length)
+{
+  char hex_characters[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+  int i;
+  srand(time(0));
+  for(i=0;i<length;i++)
+  {
+    str[i]=hex_characters[rand()%16];
+  }
+  str[length]=0;
 }
+
+void GeneratePropertiesFile(string fileName, char* key){
+  int count = 0;
+  ofstream PropertiesFile(fileName);
+  PropertiesFile << key;
+  PropertiesFile << endl;
+  PropertiesFile << count;
+  PropertiesFile.close();
+}
+
+void InitializeConnection(char* key){
+  string clientPropertiesFile = "ClientProperties.txt";
+  string serverPropertiesFile = "ServerProperties.txt";
+  GeneratePropertiesFile(clientPropertiesFile, key);
+  GeneratePropertiesFile(serverPropertiesFile, key);
+}
+
+int main(int argc, char **argv){
+  string msg = "";
+  int length = 20;
+  char key[length];
+  struct sockaddr_in server_addr;     // set server addr and port
+      memset(&server_addr, 0, sizeof(server_addr));
+      server_addr.sin_family = AF_INET;
+      server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+      server_addr.sin_port = htons(8000);  // server default port
+  int sock_client;
+  char send_buf[65536];
+      memset(send_buf, '\0', sizeof(send_buf));
+  char* send_content;
+  if(argc == 2){
+    if(strcmp(argv[1],"initialize")){
+      cout << "Initializing connection!" << endl;
+      GenerateHexString(key, length);
+      InitializeConnection(key);
+      send_content = (char*)"Initialize";
+      strcpy(send_buf, send_content);
+      strcat(send_buf, "\n");
+      strcat(send_buf, key);
+    }
+  }
+  
+  if ((sock_client = socket(AF_INET,SOCK_STREAM, 0)) < 0) {
+      return 0;
+  }
+
+    //connect server, return 0 with success, return -1 with error
+  if (connect(sock_client, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
+      perror("connect");
+      return 0;
+  }
+
+  char server_ip[INET_ADDRSTRLEN]="";
+  inet_ntop(AF_INET, &server_addr.sin_addr, server_ip, INET_ADDRSTRLEN);
+  printf("connected server(%s:%d). \n", server_ip, ntohs(server_addr.sin_port));
+
+  //send a message to server
+  send(sock_client, send_buf, strlen(send_buf), 0);
+  close(sock_client);
+
+  return 0;
+    /*std::string_view key_view{key};
+    std::string_view msg_view{msg};
+    std::cout << CalcHmacSHA(key_view, msg_view) << std::endl;*/
+}
+
--- server.cpp
+++ server.cpp
@@ -1,5 +1,12 @@
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 #include <iomanip>
 #include <iostream>
@@ -31,9 +38,57 @@ std::string CalcHmacSHA256(std::string_view decodedKey, std::string_view msg)
 }
 
 int main(int argc, char **argv){
-    std::string key = "Pokemon";
-    std::string msg = "foo";
-    std::string_view key_view{key};
-    std::string_view msg_view{msg};
-    std::cout << CalcHmacSHA256(key_view, msg_view) << std::endl;
+    int server_sockfd;		// server socket fd	
+	struct sockaddr_in server_addr;		// server info struct
+	server_addr.sin_family=AF_INET; 	// TCP/IP
+	server_addr.sin_addr.s_addr=INADDR_ANY;		// server addr--permit all connection
+	server_addr.sin_port=htons(8000); 		// server port
+
+	/* create socket fd with IPv4 and TCP protocal*/
+	if((server_sockfd=socket(PF_INET,SOCK_STREAM,0))<0) {  
+					perror("socket error");
+					return 1;
+	}
+
+	/* bind socket with server addr */
+	if(bind(server_sockfd,(struct sockaddr *)&server_addr,sizeof(struct sockaddr))<0) {
+					perror("bind error");
+					return 1;
+	}
+
+	/* listen connection request with a queue length of 20 */
+	if(listen(server_sockfd,20)<0) {
+					perror("listen error");
+					return 1;
+	}
+	printf("listen success.\n");
+
+	char recv_buf[65536];
+	memset(recv_buf, '\0', sizeof(recv_buf));
+
+	while (1) {
+		struct sockaddr_in client_addr;
+		socklen_t length = sizeof(client_addr);
+		// block on accept until positive fd or error
+		int conn = accept(server_sockfd, (struct sockaddr*)&client_addr,&length);
+		if(conn<0) {
+			perror("connect");
+			return -1;
+		}
+
+		printf("new client accepted.\n");
+
+		char client_ip[INET_ADDRSTRLEN] = "";
+		inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+
+		while(recv(conn, recv_buf, sizeof(recv_buf), 0) > 0 ){
+			printf("recv %s from client(%s:%d). \n", recv_buf, client_ip, ntohs(client_addr.sin_port));
+			memset(recv_buf, '\0', strlen(recv_buf));
+			break;
+		}
+	}
+
+	printf("closed. \n");
+	close(server_sockfd);
+	return 0;
 }
